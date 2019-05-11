fefa66d9802be24ecb506b509369f01dfa6d8b29
松下慧 <matsushitakei@K1212.local>
2019-05-12 01:28:53 +0900

    [add]ユーザーが見るページを実装
    一旦、商品ページのみの表示で実装し動作確認。次に本格的なページ実装

---
+++ app/Http/Controllers/WineController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\HTML;
+
+use App\Wine;
+
+
+class WineController extends Controller
+{
+  $cond_title = $request->cond_title;
+      //$cond_title が空白でない場合は、記事を検索して取得する
+      if ($cond_title != '') {
+          $posts = Wine::where('title', $cond_title).orderBy('updated_at', 'desc')->get();
+      } else {
+          $posts = Wine::all()->sortByDesc('updated_at');
+      }
+
+      if (count($posts) > 0) {
+          $headline = $posts->shift();
+      } else {
+          $headline = null;
+      }
+
+      //wine/index.blade.php ファイルを渡している
+      //また View テンプレートに headline、 posts、 cond_title という変数を渡している
+      return view('wine.index', ['headline' => $headline, 'posts' => $posts, 'cond_title' => $cond_title]);
+  }
+}
